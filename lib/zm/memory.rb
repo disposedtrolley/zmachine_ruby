3c4e4a6b11ed69aa505cb2f1ac3045a705a87644
James Liu <james_liu@fastmail.com>
2021-01-05 22:16:56 +1100

    tests: fix bounds checking, add write tests

--- lib/zm/memory.rb
+++ lib/zm/memory.rb
@@ -21,9 +21,26 @@ module ZM
     end
 
     def read_word(address)
-      bounds_check(address) && bounds_check(address + 1)
+      bounds_check(address)
+      bounds_check(address + 1)
 
       @content[address] << 8 | @content[address + 1]
+    end
+
+    def write_byte(address, byte)
+      bounds_check(address)
+      read_only_check(address)
+
+      @content[address] = byte
+    end
+
+    def write_word(address, word)
+      bounds_check(address)
+      bounds_check(address + 1)
+      read_only_check(address)
+
+      @content[address] = word >> 8
+      @content[address + 1] = word
     end
 
     private
@@ -35,7 +52,7 @@ module ZM
     end
 
     def read_only_check(address)
-      if address >= Memory.header[:static_memory_begin]
+      if address >= @@header[:static_memory_begin]
         raise ReadOnlyError, "Address #{address} is in the read-only region of memory"
       end
     end
--- spec/memory_spec.rb
+++ spec/memory_spec.rb
@@ -2,16 +2,13 @@ require 'spec_helper'
 require 'zm/memory'
 
 RSpec.describe ZM::Memory do
-  describe 'out of bounds checking' do
-    it 'should throw an exception if an out-of-bounds address is read' do
-      mem = ZM::Memory.new([0xfe, 0xa2, 0x19, 0x00])
-      expect { mem.read_byte(5) }.to raise_error(ZM::Memory::OutOfBoundsError, 'Address 5 is out of bounds')
-      expect { mem.read_word(4) }.to raise_error(ZM::Memory::OutOfBoundsError, 'Address 4 is out of bounds')
-    end
-  end
-
   describe 'read' do
     mem = ZM::Memory.new([0xfe, 0xa2, 0x0d, 0x19, 0x00])
+
+    it 'should throw an exception if an out-of-bounds address is read' do
+      expect { mem.read_byte(5) }.to raise_error(ZM::Memory::OutOfBoundsError, 'Address 5 is out of bounds')
+      expect { mem.read_word(4) }.to raise_error(ZM::Memory::OutOfBoundsError, 'Address 5 is out of bounds')
+    end
 
     it 'should read a byte' do
       expect(mem.read_byte(0)).to equal 0xfe
@@ -27,6 +24,25 @@ RSpec.describe ZM::Memory do
 
     it 'should read a word at the end of memory' do
       expect(mem.read_word(3)).to equal 0x1900
+    end
+  end
+
+  describe 'write' do
+    mem = ZM::Memory.new([0xfe, 0xa2, 0x0d, 0x19, 0x00])
+
+    it 'should throw an exception if an out-of-bounds address is written to' do
+      expect { mem.write_byte(5, 0x11) }.to raise_error(ZM::Memory::OutOfBoundsError, 'Address 5 is out of bounds')
+      expect { mem.write_word(4, 0xffdd) }.to raise_error(ZM::Memory::OutOfBoundsError, 'Address 5 is out of bounds')
+    end
+
+    it 'should write a byte' do
+      mem.write_byte(0, 0xff)
+      expect(mem.read_byte(0)).to equal 0xff
+    end
+
+    it 'should write a word' do
+      mem.write_word(3, 0xaabb)
+      expect(mem.read_word(3)).to equal 0xaabb
     end
   end
 end
